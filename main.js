1c032cbdd4c97b2b020461e3728701f26233a9ce
DmitriiChiricov <98124083+DmitriiChiricov@users.noreply.github.com>
2022-01-26 19:27:21 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 //Random key generator
-
 const keys = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"];
-
-
+const lshift = document.getElementById('ShiftLeft')
+const rshift = document.getElementById('rshift')
+const caps = document.getElementById("caps")
+const esc = document.getElementById("Escape")
+const ent = document.getElementById('enter')
+const bcksp = document.getElementById('Backspace')
 function GiveMeRandomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -10,9 +13,8 @@ function GiveMeRandomNumber(min, max) {
 }
 
 function GiveMeRandomKey() {
-    return  keys [GiveMeRandomNumber(0,keys.length)];
+    return keys [GiveMeRandomNumber(0, keys.length)];
 }
-
 
 //activate random key
 function ActivateRandomKey() {
@@ -21,20 +23,57 @@ function ActivateRandomKey() {
 }
 
 //-----------
-// const li = document.querySelector('li')
 window.addEventListener('keyup', (ev) => {
     const Cpress = String.fromCharCode(ev.keyCode)
     console.log(Cpress);
     const element = document.getElementById(Cpress)
-   const selectedelement =document.querySelector('.pressed')
-if(Cpress === selectedelement.innerHTML){
-selectedelement.classList.remove('pressed');
-ActivateRandomKey();
-} else {
-    element.classList.add('hit')
-    element.addEventListener('animationend', () =>{
-        element.classList.remove('hit')
-    })
-}
+    const selectedelement = document.querySelector('.pressed')
+    if (Cpress === selectedelement.innerHTML) {
+        selectedelement.classList.remove('pressed');
+        ActivateRandomKey();
+    }
+    if (ev.code == 'ShiftLeft') {
+        lshift.classList.add('hit')
+        lshift.addEventListener('animationend', () => {
+            lshift.classList.remove('hit')
+        })
+    }
+    if (ev.code === 'CapsLock') {
+        caps.classList.add('hit')
+        caps.addEventListener('animationend', () => {
+            caps.classList.remove('hit');
+        })
+    }
+    if (ev.code === 'Escape'){
+        esc.classList.add('hit')
+        esc.addEventListener('animationend',()=>{
+            esc.classList.remove('hit')
+        })
+    }
+    if (ev.code === 'Enter'){
+        ent.classList.add('hit')
+        ent.addEventListener('animationend',()=>{
+            ent.classList.remove('hit')
+        })
+    }
+    if (ev.code === 'ShiftRight'){
+        rshift.classList.add('hit')
+        rshift.addEventListener('animationend',() =>{
+            rshift.classList.remove('hit')
+        })
+    }
+    if (ev.code === 'Backspace'){
+        bcksp.classList.add('hit')
+        bcksp.addEventListener('animationend',()=>{
+            bcksp.classList.remove('hit')
+        })
+    }
+    else
+    {
+        element.classList.add('hit')
+        element.addEventListener('animationend', () => {
+            element.classList.remove('hit')
+        })
+    }
 });
 ActivateRandomKey();
